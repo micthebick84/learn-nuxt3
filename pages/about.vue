9552f12365db8ce5f5c72aa71a404f596356a243
이근호 <khlee84@hamonsoft.co.kr>
2024-10-03 11:40:23 +0900

    pinia 적용

--- pages/about.vue
+++ pages/about.vue
@@ -27,10 +27,24 @@
             <q-btn label="clear" @click="clearNuxtState()" />
           </div>
         </div>
+        <div class="q-gutter-y-sm q-mt-md">
+          <div class="text-subtitle1 text-weight-bold">
+            useCounterStore('counter')
+          </div>
+          <div>counter: {{ count }}</div>
+          <div>counter: {{ doubleCount }}</div>
+          <div>
+            <q-btn label="increment" @click="counterStore.increment()" />
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
 </template>
 <script setup lang="ts">
 const counter = useState<number>('counter', () => 1);
+// const sameCounter = useState<number>('counter');
+
+const counterStore = useCounterStore();
+const { count, doubleCount } = storeToRefs(counterStore);
 </script>
